e8b3261e8f33fbd2a8e9ec346e91c2c9364705ca
Kris Jordan <krisjordan@gmail.com>
2012-02-10 12:21:10 -0500

    Trimming to specific task added.

--- rube.js
+++ rube.js
@@ -8,7 +8,13 @@ var _       = require('underscore'),
 
 var rubefile = JSON.parse(fs.readFileSync('Rubefile.json', 'utf8').replace(/\#.*$/mg,''));
 
-var rubefileToWorkOrder = function(rubefile, cb) {
+var rpad = function(str, padString, length) {
+    while (str.length < length)
+        str = str + padString;
+    return str;
+}
+
+var rubefileToWorkOrder = function(rubefile, task, cb) {
     // Helper functions
     var curry,
         curryCB,
@@ -39,7 +45,6 @@ var rubefileToWorkOrder = function(rubefile, cb) {
         populateOutputsRecur,
         generateWorkOrder;
 
-
     // Step 0 normalize rubefile tasks
     normalizeInputs = function(taskDefn, cb) {
         // Setup _inputs
@@ -48,7 +53,7 @@ var rubefileToWorkOrder = function(rubefile, cb) {
         } else if(_.isString(taskDefn.input) || _.isObject(taskDefn.input)) {
             taskDefn._inputs = [taskDefn.input];
         } else {
-            throw "`input` required for task definitions";
+            taskDefn._inputs = [];
         }
         cb(null, taskDefn);
     };
@@ -60,7 +65,7 @@ var rubefileToWorkOrder = function(rubefile, cb) {
             globInputs.one,
             function(err, inputs) {
                 taskDefn._inputs = _.flatten(inputs);
-                if(_.all(taskDefn._inputs, _.isString)) {
+                if(taskDefn._inputs.length > 0 && _.all(taskDefn._inputs, _.isString)) {
                     var regexStr = taskDefn.input.replace(/\*/,'([^\/]*)'),
                         regex    = new RegExp(regexStr);
                     taskDefn._outputs = _.map(taskDefn._inputs, function(input){
@@ -153,9 +158,6 @@ var rubefileToWorkOrder = function(rubefile, cb) {
                                 _.all(
                                     inputTasks,
                                     function(inputTask) {
-                                        console.log('here');
-                                        console.log(inputTasks);
-                                        console.log(inputTask);
                                         return rubefile[inputTask]._processingComplete;
                                     })
                             ) {
@@ -194,7 +196,6 @@ var rubefileToWorkOrder = function(rubefile, cb) {
             if(toProcess.length === 0) {
                 cb(null, rubefile);
             } else {
-                console.log(util.inspect(rubefile,false,4));
                 var cycle = toProcess.join(", ");
                 throw "Tasks cannot create a cycle in Rubefile: " + cycle;
             }
@@ -206,17 +207,27 @@ var rubefileToWorkOrder = function(rubefile, cb) {
         // in order to run.
         var workItems = _.map(rubefile, function(task) {
             if(task.exec) {
-                if(task._outputs.length === 1) {
-                    // single command
+                if(task._outputs) {
+                    if(task._outputs.length === 1) {
+                        // single command
+                        return {
+                            task:       task.task,
+                            input:      task._inputs.length > 1 ? task._inputs : task._inputs[0],
+                            output:     task._outputs[0],
+                            exec:       task.exec,
+                            q:          false
+                        };
+                    } else {
+                        throw "exec tasks result in a single output";
+                    }
+                } else {
                     return {
                         task:       task.task,
                         input:      task._inputs.length > 1 ? task._inputs : task._inputs[0],
-                        output:     task._outputs[0],
+                        output:     { "task": task.task },
                         exec:       task.exec,
                         q:          false
                     };
-                } else {
-                    throw "exec tasks result in a single output";
                 }
             } else if(task.multiexec) {
                 // multiexec
@@ -255,8 +266,41 @@ var rubefileToWorkOrder = function(rubefile, cb) {
             });
             return item;
         });
-
         cb(null, workItems);
+    };
+
+    // var thread = function(input) {
+    //     // Pop off input argument
+    //     arguments.shift();
+    //     var fn = _.comopose(arguments);
+    //     return fn(input);
+    // }
+    //    var ancestorOutputs = thread( input,
+    //                                  curry(pluckReverse),
+    //                                  flatten,
+    //                                  unique
+    //                                );
+
+    var trimToTask = function(task, workOrder, cb) {
+        var pruned = _.filter(workOrder, function(item) {
+            return item.task === task;
+        });
+        
+        var prune = function(tasks) {
+            var ancestorOutputs = _.unique(_.flatten(_.pluck(tasks, 'input')));
+            var ancestors = _.filter(workOrder, function(item) {
+                return _.indexOf(ancestorOutputs, item.output) >= 0;
+            });
+            _.each(ancestors, function(item) {
+                pruned.push(item);
+            });
+            if(ancestors.length > 0) {
+                prune(ancestors);
+            }
+        };
+        prune(pruned);
+
+        cb(null, pruned);
     };
 
     async.waterfall(
@@ -264,30 +308,34 @@ var rubefileToWorkOrder = function(rubefile, cb) {
             curry(normalizeTasks, rubefile),
             populateOutputs,
             generateWorkOrder,
+            curry(trimToTask, task),
             curryWaterfallCB(cb)
         ],
-        curryCB(cb)
+        function() {
+            console.log("fail");
+        }
     );
 };
 
-rubefileToWorkOrder(rubefile, function(err, tasks){
-    console.log(util.inspect(tasks, false, 4));
-});
+var hasTouched = false;
 
 async.waterfall(
     [
-        async.apply(rubefileToWorkOrder, rubefile),
+        async.apply(rubefileToWorkOrder, rubefile, "all"),
         function(workorder, cb) {
             var device = new Device(workorder);
             device.scheduleWork();
             device.on('complete', function() {
                 console.log('complete');
-                device.touch('lib/b.js');
+                console.log('touching!');
+                if(!hasTouched) {
+                    device.touch('src/b.coffee');
+                    device.touch('onepage.html');
+                    device.scheduleWork();
+                    hasTouched = true;
+                }
             });
-        }// ,
-        // function(workorder) {
-        //     console.log(workorder);
-        // }
+        }
     ],
     function(err) {
         console.log(err);
@@ -300,13 +348,44 @@ var arrayify = function(value) {
 
 var Device = function(workorder) {
     var device        = this;
+    device.taskStrLen = (_.max(workorder, function(item) { return item.task.length; })).task.length;
     device.workorder  = workorder;
     device.incomplete = _.clone(workorder);
     device.completed  = function(err, item) {
         device.incomplete = _.without(device.incomplete, item);
         device.complete.push(item);
         item.q = false;
-        
+        if(err !== null) {
+            console.log("ERR " + err);
+            // remove all descendent tasks
+            // opposite of touch
+            var flagFile = function(file,skipped) {
+                // if a task generates the file, start there
+                // otherwise look for all tasks that use the touched file as an input
+                // move these and their dependents to incomplete
+                var fileIsInputOrOutput = _.filter(device.incomplete, function(item) {
+                    var input = item.input,
+                        output = item.output;
+                    return(
+                        (_.isArray(input) && _.indexOf(input, file) >= 0) ||
+                        input === file ||
+                        output === file
+                    );
+                });
+                _.each(fileIsInputOrOutput, function(item) {
+                    device.complete.push(item);
+                    device.incomplete = _.without(device.incomplete, item);
+                    skipped.push(item);
+                    flagFile(item.output,skipped);
+                });
+                return skipped;
+            };
+            var skipped = flagFile(item.output,[]);
+            var tasks = _.unique(_.pluck(skipped, 'task'));
+            console.log("Skipping dependent tasks: " + tasks.join(', '));
+        };
+        // after touching a non-fail how do we stop from running tasks which also
+        // depend on a fail?
         if(device.incomplete.length === 0) {
             this.emit('complete');
         } else {
@@ -314,31 +393,36 @@ var Device = function(workorder) {
         }
     };
     device.q          = async.queue(function(item, cb) {
-        console.log('begin:    ' + item.exec);
         setTimeout(function() {
-            console.log('complete: ' + item.exec);
-            device.completed(null, item);
-            cb();
+            if(item.input === 'lib/b.js') {
+                console.log('failed: ' + item.exec);
+                device.completed("fail",item);
+                cb();
+            } else {
+                console.log(rpad(item.task,' ',device.taskStrLen) + ' | ' + item.exec);
+                device.completed(null, item);
+                cb();
+            }
         }, 100);
-    }, 4);
+    }, 2);
     device.complete   = [];
-    device.touch = function(file) {
-        // do something
-        // cases: source or output file
-        var inputTasks  = [],
-            outputTasks = [];
-        _.each(device.workorder, function(item) {
-            var inputs = arrayify(item.input);
-            if(_.indexOf(inputs, file) >= 0) {
-                inputTasks.push(item);
-            }
-            if(_.isEqual(item.output, file)) {
-                outputTasks.push(item);
-            }
-        });
-        console.log(inputTasks);
-        console.log(outputTasks);
-    };
+    // device.touch = function(file) {
+    //     // do something
+    //     // cases: source or output file
+    //     var inputTasks  = [],
+    //         outputTasks = [];
+    //     _.each(device.workorder, function(item) {
+    //         var inputs = arrayify(item.input);
+    //         if(_.indexOf(inputs, file) >= 0) {
+    //             inputTasks.push(item);
+    //         }
+    //         if(_.isEqual(item.output, file)) {
+    //             outputTasks.push(item);
+    //         }
+    //     });
+    //     console.log(inputTasks);
+    //     console.log(outputTasks);
+    // };
 };
 Device.fn = Device.prototype = new events.EventEmitter;
 Device.fn.scheduleWork = function() {
@@ -379,4 +463,32 @@ Device.fn.scheduleWork = function() {
             throw "Error processing queue.";
         }
     }
+};
+Device.fn.touch = function(files) {
+    files = arrayify(files);
+    console.log(files);
+
+    var device = this;
+
+    var touchFile = function(file) {
+        // if a task generates the file, start there
+        // otherwise look for all tasks that use the touched file as an input
+        // move these and their dependents to incomplete
+        var fileIsInputOrOutput = _.filter(device.complete, function(item) {
+            var input = item.input,
+                output = item.output;
+            return(
+                (_.isArray(input) && _.indexOf(input, file) >= 0) ||
+                input === file ||
+                output === file
+            );
+        });
+        _.each(fileIsInputOrOutput, function(item) {
+            device.incomplete.push(item);
+            device.complete = _.without(device.complete, item);
+            touchFile(item.output);
+        });
+    };
+
+    _.each(files, touchFile);
 };
